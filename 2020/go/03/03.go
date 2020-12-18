dcbf32abbb70e0fb3dfc58f4ae88120abc58e2ec
contre95 <lucas.contreras@mercadolibre.com>
2020-12-17 22:53:37 -0300

    03py:snake:

--- 2020/go/03/03.go
+++ 2020/go/03/03.go
@@ -27,26 +27,31 @@ func fileToSlice(fp string) ([]string, error) {
 func count_trees(path []string, du int, rl int) int {
 	l1 := len(path[0])
 	l2 := len(path)
+	fmt.Printf("%d - %d\n", l1, l2)
 	j := 0
 	i := 0
 	trees := 0
 	for {
-		if j+du >= l2 {
+		fmt.Printf("%d - %d\n", j, i)
+		fmt.Printf("%v\n ", path[j])
+		j = j + du
+		if j >= l2 {
 			return trees
 		}
 		i = i + rl
 		if i >= l1 {
 			i = i - l1
 		}
-		j = j + du
 		if string(path[j][i]) == "#" {
 			trees++
+			fmt.Println("#")
 		}
 	}
 }
 
 func main() {
-	path, err := fileToSlice("input.txt")
+	path, err := fileToSlice("example.txt")
+	//path, err := fileToSlice("input.txt")
 	check(err)
 	trees := count_trees(path, 1, 3)
 	fmt.Printf("Number of trees in the path: %d \n", trees)
--- 2020/python/03/03.py
+++ 2020/python/03/03.py
@@ -1,10 +1,33 @@
-def mifuncionlova():
+def inputParser(path:str) -> list:
     lines = []
-    with open('./input.txt') as f:
+    with open(path) as f:
         for l in f:
-            lines.append(l)
-
-mifuncionlova():
-
+            lines.append(l[:-2])
+    return lines
 
 
+
+def main():
+    levels =  inputParser("input2.txt") # Matrix
+    lv = 0
+    i = 0
+    cant_levels = len(levels)
+    largo_lv = len(levels[0]) 
+    print(levels[0]) 
+    print(f"{cant_levels} - {largo_lv}")
+    trees = 0
+    while True:
+        print(f"{lv} - {i}")
+        print(levels[lv])
+        lv= lv + 1
+        if  lv >= cant_levels:
+            return trees
+        i += 3
+        if i >= largo_lv:
+            i = i - largo_lv
+        if levels[lv][i] == "#":
+            trees = trees + 1
+            print("#")
+
+if __name__ == "__main__" :
+    print(main())
