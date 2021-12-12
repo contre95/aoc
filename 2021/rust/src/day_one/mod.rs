f30ed1e625f43cace0f9f1988905b100ad11ca00
Lucas Contreras <lucascontre95@gmail.com>
2021-12-12 22:16:29 +0100

    Day 1 part2

---
+++ 2021/rust/src/day_one/mod.rs
@@ -0,0 +1,45 @@
+use std::fs;
+use std::io::{BufRead, BufReader};
+
+fn lines_to_vector(path: &str) -> Vec<u32> {
+    let mut input: Vec<u32> = vec![];
+    let file = fs::File::open(path).expect("Error while reading the file");
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let line = line.expect("Unable to read line");
+        input.push(line.parse().expect("Could not parse string to u32"))
+    }
+    return input;
+}
+
+pub fn part1() -> u32 {
+    let input = lines_to_vector("src/day_one/input");
+    let mut count = 0;
+    let mut increase: u32 = 0;
+    loop {
+        let n1: u32 = input[count];
+        let n2: u32 = input[count + 1];
+        if n1 < n2 {
+            increase += 1;
+        }
+        count += 1;
+        if count + 1 >= input.len() {
+            break increase;
+        }
+    }
+}
+
+pub fn part2() -> u32 {
+    let input = lines_to_vector("src/day_one/input");
+    let mut count = 0;
+    let mut increase: u32 = 0;
+    while count + 4 <= input.len() {
+        let n1: u32 = input[count] + input[count + 1] + input[count + 2];
+        let n2: u32 = input[count + 1] + input[count + 2] + input[count + 3];
+        if n1 < n2 {
+            increase += 1;
+        }
+        count += 1;
+    }
+    increase
+}
---
+++ 2021/rust/src/main.rs
@@ -0,0 +1,7 @@
+mod day_one;
+
+fn main() {
+    println!("Day 1");
+    println!("Part 1: {}", day_one::part1());
+    println!("Part 2: {}", day_one::part2());
+}
