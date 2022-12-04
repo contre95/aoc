78ae3c058ea05e7a601d2208ceab478c873eb8db
contre95 <lucascontre95@gmail.com>
2022-12-04 18:30:04 +0100

    Nvim Debugger UI setup

---
+++ 2022/go/04/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func fileToA(filePath string) [][]int {
+	var a [][]int
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Could not read the file due to this %s error \n", err)
+	}
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+	for fileScanner.Scan() {
+		line := strings.Split(fileScanner.Text(), ",")
+		p1a, _ := strconv.Atoi(strings.Split(line[0], "-")[0])
+		p1b, _ := strconv.Atoi(strings.Split(line[0], "-")[1])
+		p2a, _ := strconv.Atoi(strings.Split(line[1], "-")[0])
+		p2b, _ := strconv.Atoi(strings.Split(line[1], "-")[1])
+		a = append(a, []int{p1a, p1b, p2a, p2b})
+	}
+	if err = file.Close(); err != nil {
+		fmt.Printf("Could not close the file due to this %s error \n", err)
+	}
+	return a
+}
+
+func main() {
+	input := fileToA(os.Args[1])
+	count := 0
+	for _, v := range input {
+		if v[2] >= v[0] && v[3] <= v[1] {
+			count += 1
+		}
+		if v[2] <= v[0] && v[3] >= v[1] {
+			count += 1
+		}
+	}
+	fmt.Println(count)
+}
