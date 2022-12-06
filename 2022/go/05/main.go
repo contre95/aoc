74748a4ce976ac515c5d2f2a6ad6a4062b203245
contre95 <lucascontre95@gmail.com>
2022-12-06 20:06:33 +0100

    Parsing of input day5

--- 2022/go/05/main.go
+++ 2022/go/05/main.go
@@ -24,19 +24,21 @@ func inputParser(filePath string) (map[int]string, [][]int) {
 		}
 		count := 1
 		for i := 1; i < len(line); i += 4 {
-			if _, ok := stacks[count]; ok {
-				stacks[count] = string(line[i]) + stacks[count]
-			} else {
-				stacks[count] = string(line[i])
+			if string(line[i]) != "" {
+				if _, ok := stacks[count]; ok {
+					stacks[count] = strings.Replace(string(line[i]), " ", "", -1) + stacks[count]
+				} else {
+					stacks[count] = strings.Replace(string(line[i]), " ", "", -1)
+				}
+				count++
 			}
-			count++
 		}
 	}
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
-		r := strings.NewReplacer("move", "", "from", "", "to", "", " ", "")
+		r := strings.NewReplacer("move", "", "from", "", "to", "")
 		op := []int{}
-		for _, v := range r.Replace(line) {
+		for _, v := range strings.Fields(r.Replace(line)) {
 			i, _ := strconv.Atoi(string(v))
 			op = append(op, i)
 		}
@@ -45,10 +47,24 @@ func inputParser(filePath string) (map[int]string, [][]int) {
 	if err = file.Close(); err != nil {
 		fmt.Printf("Could not close the file due to this %s error \n", err)
 	}
-	return stacks, ops[1 : len(ops)-2]
+	return stacks, ops[1 : len(ops)-1]
 }
 
 func main() {
 	stacks, ops := inputParser(os.Args[1])
-	fmt.Println(stacks, ops)
+	for _, op := range ops {
+		move := op[0]
+		from := op[1]
+		to := op[2]
+		//fmt.Printf("%v :\n Move %d from %d (%v) to %d (%v) \n", stacks, move, from, stacks[from], to, stacks[to])
+		buff := stacks[from][len(stacks[from])-move:]
+		for i := len(buff) - 1; i >= 0; i-- {
+			stacks[to] += string(stacks[from][len(stacks[from])-move:][i])
+		}
+		stacks[from] = stacks[from][:len(stacks[from])-move]
+	}
+	for _, stack := range stacks {
+		fmt.Printf("%c", stack[len(stack)-1])
+	}
+
 }
